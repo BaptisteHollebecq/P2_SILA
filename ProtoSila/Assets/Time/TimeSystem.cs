9f1a1b9d7aec1149164e921a5cecc2059e3ca7a9
BaptisteHollebecq <baptiste.hollebecq@pole3d.com>
2020-01-23 13:39:34 +0100

    unity project

---
+++ ProtoSila/Assets/Time/TimeSystem.cs
@@ -0,0 +1,242 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeSystem : MonoBehaviour
+{
+    public enum TimeOfDay { Morning, Day, Noon, Night, Null };
+
+    [SerializeField] private Transform _lightTransform;
+    [SerializeField] private Light _light;
+
+    [Header("")]
+    public TimeOfDay startingTime;
+    private TimeOfDay _targetTime = TimeOfDay.Null;
+    [HideInInspector] public TimeOfDay actualTime;
+    [HideInInspector] public float currentTime;                 // current time used in transition
+    [SerializeField] private float _transitionTime = 2f;        // time in second to go from the actual time to the next one
+    
+
+    [Header("Morning Setup")]
+    [SerializeField] private float _lightIntensityMorning;
+    [SerializeField] private Vector3 _sunRotationMorning;
+    [SerializeField] private Gradient _colorMorning;
+
+    [Header("Day Setup")]
+    [SerializeField] private float _lightIntensityDay;
+    [SerializeField] private Vector3 _sunRotationDay;
+    [SerializeField] private Gradient _colorDay;
+
+    [Header("Noon Setup")]
+    [SerializeField] private float _lightIntensityNoon;
+    [SerializeField] private Vector3 _sunRotationNoon;
+    [SerializeField] private Gradient _colorNoon;
+
+    [Header("Night Setup")]
+    [SerializeField] private float _lightIntensityNight;
+    [SerializeField] private Vector3 _sunRotationNight;
+    [SerializeField] private Gradient _colorNight;
+
+    private float _transitionScale;
+    private float _transitionSlide = 0f;
+    private float _rotationScale;
+    private float _intensityScale;
+    private float _timeScale;
+
+    void MorningTime()
+    {
+        _lightTransform.rotation = Quaternion.Euler(_sunRotationMorning);
+        _light.intensity = _lightIntensityMorning;
+        _light.color = _colorMorning.Evaluate(0);
+        actualTime = TimeOfDay.Morning;
+        currentTime = 0f;
+    }
+
+    void DayTime()
+    {
+        _lightTransform.rotation = Quaternion.Euler(_sunRotationDay);
+        _light.intensity = _lightIntensityDay;
+        _light.color = _colorDay.Evaluate(0);
+        actualTime = TimeOfDay.Day;
+        currentTime = 0.25f;
+    }
+
+    void NoonTime()
+    {
+        _lightTransform.rotation = Quaternion.Euler(_sunRotationNoon);
+        _light.intensity = _lightIntensityNoon;
+        _light.color = _colorNoon.Evaluate(0);
+        actualTime = TimeOfDay.Noon;
+        currentTime = 0.5f;
+    }
+
+    void NightTime()
+    {
+        _lightTransform.rotation = Quaternion.Euler(_sunRotationNight);
+        _light.intensity = _lightIntensityNight;
+        _light.color = _colorNight.Evaluate(0);
+        actualTime = TimeOfDay.Night;
+        currentTime = 0.75f;
+    }
+
+    void SetupScale(TimeOfDay from)
+    {
+        if (from == TimeOfDay.Morning)
+        {
+            _rotationScale = ((_sunRotationDay.x - _sunRotationMorning.x) / _transitionTime) * Time.deltaTime;
+            _transitionScale = (1 / _transitionTime) * Time.deltaTime;
+            _timeScale = (0.25f / _transitionTime) * Time.deltaTime;
+            _intensityScale = (Mathf.Abs(_lightIntensityMorning - _lightIntensityDay) / _transitionTime) * Time.deltaTime;
+        }
+        else if (from == TimeOfDay.Day)
+        {
+            _rotationScale = ((_sunRotationNoon.x - _sunRotationDay.x) / _transitionTime) * Time.deltaTime;
+            _transitionScale = (1 / _transitionTime) * Time.deltaTime;
+            _timeScale = (0.25f / _transitionTime) * Time.deltaTime;
+            _intensityScale = (Mathf.Abs(_lightIntensityDay - _lightIntensityNoon) / _transitionTime) * Time.deltaTime;
+        }
+        else if (from == TimeOfDay.Noon)
+        {
+            _rotationScale = ((360 + (_sunRotationNight.x - _sunRotationNoon.x) - 180) / _transitionTime) * Time.deltaTime;
+            _transitionScale = (1 / _transitionTime) * Time.deltaTime;
+            _timeScale = (0.25f / _transitionTime) * Time.deltaTime;
+            _intensityScale = (Mathf.Abs(_lightIntensityNoon - _lightIntensityNight) / _transitionTime) * Time.deltaTime;
+        }
+        else if (from == TimeOfDay.Night)
+        {
+            _rotationScale = ((360 - (_sunRotationMorning.x - _sunRotationNight.x) - 180) / 3.5f) * Time.deltaTime;
+            _transitionScale = (1 / 3.5f) * Time.deltaTime;
+            _timeScale = (0.25f / 3.5f) * Time.deltaTime;
+            _intensityScale = (Mathf.Abs(_lightIntensityMorning - _lightIntensityNight) / 3.5f) * Time.deltaTime;
+        }
+    }
+
+    TimeOfDay ChangeTime(TimeOfDay from, TimeOfDay to)
+    {
+        SetupScale(from);
+
+        if (from != to)
+        {
+            if (_transitionSlide <= 1)
+            {
+                _transitionSlide += _transitionScale;
+                if (from == TimeOfDay.Morning)
+                {
+                    _light.color = _colorMorning.Evaluate(_transitionSlide);
+                    if (_lightIntensityDay > _lightIntensityMorning)
+                        _light.intensity += _intensityScale;
+                    else
+                        _light.intensity -= _intensityScale;
+                }
+                else if (from == TimeOfDay.Day)
+                {
+                    _light.color = _colorDay.Evaluate(_transitionSlide);
+                    if (_lightIntensityNoon > _lightIntensityDay)
+                        _light.intensity += _intensityScale;
+                    else
+                        _light.intensity -= _intensityScale;
+                }
+                else if (from == TimeOfDay.Noon)
+                {
+                    _light.color = _colorNoon.Evaluate(_transitionSlide);
+                    if (_lightIntensityNight > _lightIntensityNoon)
+                        _light.intensity += _intensityScale;
+                    else
+                        _light.intensity -= _intensityScale;
+                }
+                else if (from == TimeOfDay.Night)
+                {
+                    _light.color = _colorNight.Evaluate(_transitionSlide);
+                    if (_lightIntensityMorning > _lightIntensityNight)
+                        _light.intensity += _intensityScale;
+                    else
+                        _light.intensity -= _intensityScale;
+                }
+                currentTime += _timeScale;
+                if (currentTime >= 1)
+                    currentTime -= 1;
+                _lightTransform.Rotate(new Vector3(1f, 0f, 0f), _rotationScale);
+                if ((_lightTransform.rotation.x >= 0.999987f || _lightTransform.rotation.x < 0f))
+                {
+                    _lightTransform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+                    //Debug.Log(_transitionSlide);
+                }
+                return from;
+            }
+            else
+            {
+                if (from == TimeOfDay.Morning)
+                    from = TimeOfDay.Day;
+                else if (from == TimeOfDay.Day)
+                    from = TimeOfDay.Noon;
+                else if (from == TimeOfDay.Noon)
+                    from = TimeOfDay.Night;
+                else if (from == TimeOfDay.Night)
+                    from = TimeOfDay.Morning;
+                _transitionSlide = 0;
+                return from;
+            }
+        }
+        else
+        {
+            if (from == TimeOfDay.Morning)
+                MorningTime();
+            else if (from == TimeOfDay.Day)
+                DayTime();
+            else if (from == TimeOfDay.Noon)
+                NoonTime();
+            else if (from == TimeOfDay.Night)
+                NightTime();
+            _transitionSlide = 0;
+            _targetTime = TimeOfDay.Null;
+            return from;
+        }
+    }
+
+    private void Start()
+    {
+        switch (startingTime)
+        {
+            case TimeOfDay.Morning:
+                MorningTime();
+                break;
+            case TimeOfDay.Day:
+                DayTime();
+                break;
+            case TimeOfDay.Noon:
+                NoonTime();
+                break;
+            case TimeOfDay.Night:
+                NightTime();
+                break;
+            default:
+                MorningTime();
+                break;
+        }
+        Debug.Log("test git modif");
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Night"))
+            _targetTime = TimeOfDay.Night;
+        if (Input.GetButtonDown("Morning"))
+            _targetTime = TimeOfDay.Morning;
+        if (Input.GetButtonDown("Day"))
+            _targetTime = TimeOfDay.Day;
+        if (Input.GetButtonDown("Noon"))
+            _targetTime = TimeOfDay.Noon;
+
+        //Debug.Log(actualTime);
+        //Debug.Log(_rotationScale);
+        //Debug.Log(_lightTransform.localRotation.x);
+        //Debug.Log(currentTime);
+    }
+    private void FixedUpdate()
+    {
+        if (_targetTime != TimeOfDay.Null)
+        {
+            actualTime = ChangeTime(actualTime, _targetTime);
+        }
+    }
+}
---
+++ ProtoSila/Assets/Time/Timelapse.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timelapse : MonoBehaviour
+{
+
+    [Header("TIME")]
+    [Tooltip("Day Lenght in minutes")]
+    [SerializeField]
+    private float _irlDayLength = 1f;
+    public float irlDayLength { get { return _irlDayLength; } }
+
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float _timeOfDay;
+    public float timeOfDay { get { return _timeOfDay; } }
+    public bool pause = false;
+
+    [Header("SETUP")]
+    public float accelerationTime = 10f;
+    [Header("")]
+    public float nightTime = 0f;
+    public float morningTime = 6f;
+    public float dayTime = 12f;
+    public float noonTime = 18f;
+
+    [Header("SUN")]
+    [SerializeField]
+    private Transform _sun;
+    [SerializeField]
+    private Light _sunLight;
+    private float _sunIntensity;
+    [SerializeField]
+    private float _sunVariation = 1.5f;
+    [SerializeField]
+    private float _sunBaseIntensity = 1f;
+
+    private float _timeScale = 100f;
+    private float _refTimeBase = 60f;
+    private float _refTime = 60f;
+    private float _targetTime = -1;
+
+    private void Start()
+    {
+        CheckTime();
+    }
+
+    void CheckTime()
+    {
+        if (nightTime == 24)
+            nightTime = 0;
+        if (morningTime == 24)
+            morningTime = 0;
+        if (dayTime == 24)
+            dayTime = 0;
+        if (noonTime == 24)
+            noonTime = 0;
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetButtonDown("Night"))
+            _targetTime = nightTime;
+        if (Input.GetButtonDown("Morning"))
+            _targetTime = morningTime;
+        if (Input.GetButtonDown("Day"))
+            _targetTime = dayTime; 
+        if (Input.GetButtonDown("Noon"))
+            _targetTime = noonTime;
+
+
+        if (_targetTime != -1)
+        {
+            _refTime = _refTimeBase * (accelerationTime);
+            if (Mathf.Floor(_timeOfDay * 24) == _targetTime)
+            {
+                _targetTime = -1;
+                _refTime = _refTimeBase;
+            }
+        }
+
+        Debug.Log(Mathf.Floor(_timeOfDay * 24));
+    }
+
+    private void FixedUpdate()
+    {
+        if (!pause)
+        {
+            UpdateTimeScale();
+            UpdateTime();
+        }
+        AdjustSunRotation();
+        SunIntensity();
+    }
+
+
+    private void UpdateTimeScale()
+    {
+        _timeScale = 24 / (_irlDayLength / _refTime);
+    }
+
+    private void UpdateTime()
+    {
+        _timeOfDay += Time.deltaTime * _timeScale / 86400;
+        if (_timeOfDay > 1)
+            _timeOfDay -= 1;
+    }
+
+    private void AdjustSunRotation()
+    {
+        float sunAngle = (_timeOfDay * 360f);
+        _sun.transform.localRotation = Quaternion.Euler(new Vector3(sunAngle, 0f, 0f));
+    }
+
+    private void SunIntensity()
+    {
+        /*float _morningLength = (dayTime - morningTime);
+        float _intensityScaleMorning = (_sunVariation / _timeScale) * _morningLength;
+
+        float _noonLength = (24 - noonTime + nightTime);
+        float _intensityScaleNoon = (_sunVariation / _timeScale) * _noonLength;
+
+
+        if ((_timeOfDay >= (noonTime / 24) && _timeOfDay < 24) || (_timeOfDay >= 0 && _timeOfDay <= (nightTime / 24)))
+            _sunLight.intensity -= _intensityScaleNoon;
+        if (_timeOfDay >= (morningTime / 24) && _timeOfDay <= (dayTime / 24))
+            _sunLight.intensity += _intensityScaleMorning;*/
+
+        #region OLD
+        _sunIntensity = Vector3.Dot(_sunLight.transform.forward, Vector3.down);
+        _sunIntensity = Mathf.Clamp01(_sunIntensity);
+
+        _sunLight.intensity = _sunIntensity * _sunVariation + _sunBaseIntensity;
+        #endregion
+    }
+
+}
