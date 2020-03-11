f6b201850e39af3323273c78ccd93afc9aaf3883
BaptisteHollebecq <baptiste.hollebecq@pole3d.com>
2020-03-11 10:25:16 +0100

    update

---
+++ SILA/Assets/Scripts/Plateforms/DeathTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(BoxCollider))]
+public class DeathTest : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
