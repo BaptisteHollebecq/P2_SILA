0b6862f3536c3f8954fc2d06472c1976b1051567
Loïc_Mry <loicmry@gmail.com>
2020-03-23 01:50:44 +0100

    Start Enum

---
+++ SILA/Assets/Scripts/Player/_PlayerControllerV2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum States
+{
+	Grounded,
+	Jumping,
+	Dashing,
+	Flying,
+	OnStele,
+	Zoom,
+}
+public class _PlayerControllerV2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
