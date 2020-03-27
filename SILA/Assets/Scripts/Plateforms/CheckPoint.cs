5a28d8b268162881a3e7ee88d972c167ffb5519c
BaptisteHollebecq <baptiste.hollebecq@pole3d.com>
2020-03-27 09:31:15 +0100

    update death system

---
+++ SILA/Assets/Scripts/Plateforms/CheckPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+
+    private PlayerLifeManager _lifeManager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _lifeManager = other.GetComponent<PlayerLifeManager>();
+            _lifeManager.CheckPoint();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _lifeManager = null;
+        }
+    }
+}
