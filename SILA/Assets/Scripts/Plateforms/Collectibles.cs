43f6198b5d028fe840861fcce24ebd1e3d2871d1
BaptisteHollebecq <baptiste.hollebecq@pole3d.com>
2020-04-10 11:48:25 +0200

    start main menu

---
+++ SILA/Assets/Scripts/Plateforms/Collectibles.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Collider))]
+public class Collectibles : MonoBehaviour
+{
+    PlayerCollectibles _collectibles;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _collectibles = other.GetComponent<PlayerCollectibles>();
+            _collectibles.Add(1);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ SILA/Assets/Scripts/Player/PlayerCollectibles.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollectibles : MonoBehaviour
+{
+    private int _collectibles = 0;
+
+    public void Add(int nbr)
+    {
+        _collectibles+=nbr;
+    }
+}
