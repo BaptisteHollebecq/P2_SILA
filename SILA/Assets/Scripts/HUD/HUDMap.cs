5632e1089ea815be39e058a578d82a28f1a205a7
BaptisteHollebecq <baptiste.hollebecq@pole3d.com>
2020-05-14 09:12:03 +0200

    map2/2

--- SILA/Assets/Scripts/HUD/HUDMap.cs
+++ SILA/Assets/Scripts/HUD/HUDMap.cs
@@ -7,15 +7,22 @@ public class HUDMap : MonoBehaviour
 {
     [SerializeField] private PlayerControllerV2 _player;
     [SerializeField] private List<Zone> _zones;
+    [SerializeField] private Transform _allZones;
 
 
     private Transform _canvas;
     private bool _isOpen = false;
+    private bool _isZoomed = false;
 
     public void ChangeZone(int entered, int exited)
     {
         _zones[entered - 1].Entered();
         _zones[exited - 1].PlayerExit();
+    }
+
+    public void Collect(int zone)
+    {
+        _zones[zone - 1]._collectibles--;
     }
 
     private void Awake()
@@ -37,8 +44,38 @@ public class HUDMap : MonoBehaviour
         {
             _isOpen = false;
             _canvas.gameObject.SetActive(false);
+            _allZones.localScale = new Vector3(1, 1, 1);
+            _allZones.localPosition = Vector3.zero;
             Time.timeScale = 1;
+            _isZoomed = false;
             _player._isOnMap = false;
+        }
+        if (Input.GetButtonDown("A") && _isOpen)
+        {
+            if (!_isZoomed)
+            {
+                _isZoomed = true;
+                _allZones.localScale = new Vector3(2, 2, 2);
+            }
+            else
+            {
+                _isZoomed = false;
+                _allZones.localScale = new Vector3(1, 1, 1);
+                _allZones.localPosition = Vector3.zero;
+            }
+        }
+        if (_isZoomed && _isOpen)
+        {
+            Vector2 stickInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            if (stickInput.magnitude < 0.25f)
+                stickInput = Vector2.zero;
+            else
+            {
+                Vector3 tmp = _allZones.localPosition - (new Vector3(stickInput.x, stickInput.y, 0) * 15);
+                tmp.x = Mathf.Clamp(tmp.x, -500, 500);
+                tmp.y = Mathf.Clamp(tmp.y, -500, 500);
+                _allZones.localPosition = tmp;
+            }
         }
     }
 }
