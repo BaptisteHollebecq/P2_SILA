e8ec2c59a8d0c242447467e68ac75fff6e42cf63
rcq-rcq <leclercq.guillaume08@gmail.com>
2020-06-01 04:38:35 +0200

    Lake integré

--- SILA/Assets/Scripts/Shader/Water/Glaciation.cs
+++ SILA/Assets/Scripts/Shader/Water/Glaciation.cs
@@ -15,16 +15,29 @@ public class Glaciation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        glace = Mathf.Abs(2 * (TimeSystem.currentTime - 0.25f));
+        //glace = Mathf.Clamp(TimeSystem.currentTime,0.5f,1);
 
-        if (glace < 1)
+        if (TimeSystem.currentTime > 0.5f && TimeSystem.currentTime < 0.75f)
         {
-            glace = (Mathf.Repeat(glace, 1));
+            glace = TimeSystem.currentTime;
         }
-        else
+        else if (TimeSystem.currentTime >= 0.75f && TimeSystem.currentTime < 1)
         {
-            glace = 1 - (Mathf.Repeat(glace, 1));
+            glace = 1.5f - TimeSystem.currentTime;
         }
+       
+        glace = (glace - 0.5f) * 4;
+
+        //glace = Mathf.Abs(2 * (TimeSystem.currentTime - 0.25f));
+
+        //if (glace < 1)
+        //{
+        //    glace = (Mathf.Repeat(glace, 1));
+        //}
+        //else
+        //{
+        //    glace = 1 - (Mathf.Repeat(glace, 1));
+        //}
         
         gameObject.GetComponent<Renderer>().sharedMaterial.SetFloat("_Glaciation", glace);
     }
