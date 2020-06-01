4877d112ac8b0104f33c52c04e834b55ffc312c8
rcq-rcq <leclercq.guillaume08@gmail.com>
2020-06-01 20:28:14 +0200

    SnowFall

--- SILA/Assets/Scripts/Vfx/SnowFall.cs
+++ SILA/Assets/Scripts/Vfx/SnowFall.cs
@@ -6,6 +6,21 @@ public class SnowFall : MonoBehaviour
 {
     ParticleSystem _particleSystem;
 
+    [SerializeField]
+    private float _particleSpeed;
+    [SerializeField]
+    private float _particleNumber;
+    [SerializeField]
+    private float _particleLife;
+
+    [SerializeField]
+    private float _transitionParticleSpeed;
+    [SerializeField]
+    private float _transitionParticleNumber;
+    [SerializeField]
+    private float _transitionParticleLife;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +30,18 @@ public class SnowFall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (TimeSystem._transitionSlide != 0)
+        {
+
+            _particleSystem.startSpeed = _transitionParticleSpeed;
+            _particleSystem.emissionRate = _transitionParticleNumber;
+            _particleSystem.startLifetime = _transitionParticleLife;
+        }
+        else
+        {
+            _particleSystem.startSpeed = _particleSpeed;
+            _particleSystem.emissionRate = _particleNumber;
+            _particleSystem.startLifetime = _particleLife;
+        }
     }
 }
