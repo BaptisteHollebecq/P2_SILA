8eec9df4fc0f8ecf9b7d4ebb2bbbb498ff898804
rcq-rcq <leclercq.guillaume08@gmail.com>
2020-06-01 19:33:40 +0200

    Geyser et particle system

---
+++ SILA/Assets/Scripts/Vfx/SnowFall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowFall : MonoBehaviour
+{
+    ParticleSystem _particleSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
