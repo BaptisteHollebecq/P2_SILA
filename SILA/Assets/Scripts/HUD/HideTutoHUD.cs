69b77b7e63c4313406fa5f0c77ac16ecbfbb3702
BaptisteHollebecq <55829266+BaptisteHollebecq@users.noreply.github.com>
2020-08-17 12:00:22 +0200

    tuto

---
+++ SILA/Assets/Scripts/HUD/HideTutoHUD.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideTutoHUD : MonoBehaviour
+{
+    public static event System.Action<string> EnteredZoneHide;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            EnteredZoneHide?.Invoke("");
+        }
+    }
+}
---
+++ SILA/Assets/Scripts/HUD/ShowTutoHUD.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowTutoHUD : MonoBehaviour
+{
+    public static event System.Action<string> EnteredZoneShow;
+
+    public string tutoText;
+    public SoundManager sound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            EnteredZoneShow?.Invoke(tutoText);
+            sound.Play("tuto");
+        }
+    }
+}
